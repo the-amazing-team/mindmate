0299386f0d12c7f7adc136ddb0ec99b940e610aa
dusklight00 <r.rahul.developer@gmail.com>
2026-06-13 16:17:20 +0530

    clean: remove unused api directory

--- api/src/users/dto/google-auth.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-
-export class GoogleAuthDto {
-  @ApiProperty({ example: "eyJhbGciOiJSUzI1NiIsImtpZCI6Ij..." })
-  idToken: string;
-
-  @ApiPropertyOptional({ example: "android" })
-  platform?: "android" | "ios" | "web";
-}
